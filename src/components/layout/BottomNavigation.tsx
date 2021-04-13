860cb1e0a87e4c55a00fb68a44b9f43855c2f26f
ruddenchaux <f.r.dainelli@gmail.com>
2021-04-13 22:54:18 +0200

    feat(characters): added static characters grid cards

--- src/components/layout/BottomNavigation.tsx
+++ src/components/layout/BottomNavigation.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import itemsMenu from '../../utils/itemsMenu';
 
-// style of component
+// component style
 const useStyles = makeStyles(() => ({
   bottomNavigation: {
     position: 'fixed',
