2e252148585ae6099d8eeeced5dd3794779b90ea
ruddenchaux <f.r.dainelli@gmail.com>
2021-04-18 16:32:56 +0200

    feat(favorites): added persistence for favorites store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@rtk-incubator/rtk-query';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { charactersApi } from '../services/characters';
 import { episodesApi } from '../services/episodes';
 import { locationsApi } from '../services/locations';
@@ -7,27 +9,39 @@ import { charactersDialogSlice } from './charactersDialog';
 import { favoritesSlice } from './favorites';
 import { headerTitleSlice } from './headerTitle';
 
+// persist reducer
+const persistConfig = {
+  key: 'favorites',
+  version: 1,
+  storage,
+  whitelist: ['favorites']
+};
+
+// reducer
+const rootReducer = combineReducers({
+  [charactersApi.reducerPath]: charactersApi.reducer,
+  [locationsApi.reducerPath]: locationsApi.reducer,
+  [episodesApi.reducerPath]: episodesApi.reducer,
+  headerTitle: headerTitleSlice.reducer,
+  charactersDialog: charactersDialogSlice.reducer,
+  favorites: favoritesSlice.reducer
+});
+
 const store = configureStore({
-  reducer: {
-    // Add the generated reducer as a specific top-level slice
-    [charactersApi.reducerPath]: charactersApi.reducer,
-    [locationsApi.reducerPath]: locationsApi.reducer,
-    [episodesApi.reducerPath]: episodesApi.reducer,
-    headerTitle: headerTitleSlice.reducer,
-    charactersDialog: charactersDialogSlice.reducer,
-    favorites: favoritesSlice.reducer
-  },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
+  reducer: persistReducer(persistConfig, rootReducer),
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
       .concat(charactersApi.middleware)
       .concat(locationsApi.middleware)
       .concat(episodesApi.middleware)
 });
 
+export const persistor = persistStore(store);
+
 export default store;
 
-// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
-// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
