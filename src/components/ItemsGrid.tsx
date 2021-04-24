6416092d1f2b1f0b51f518ad2826b0bc4d8c3c24
ruddenchaux <f.r.dainelli@gmail.com>
2021-04-24 14:15:12 +0200

    fix(infinite-loading): fixed a infinite loading behavior on quickly srcoll
    fix #14

--- src/components/ItemsGrid.tsx
+++ src/components/ItemsGrid.tsx
@@ -1,8 +1,27 @@
 import { Container, Grid, GridSize } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
-import React, { LegacyRef } from 'react';
-import useGrid, { UseGridOptions } from '../hooks/useGrid';
+import { QueryDefinition } from '@rtk-incubator/rtk-query/dist';
+import { UseQuery } from '@rtk-incubator/rtk-query/dist/ts/react-hooks/buildHooks';
+import React, { LegacyRef, useEffect, useState } from 'react';
 import useGridStyle from '../hooks/useGridStyle';
+import useIntersectionObserver from '../hooks/useIntersectionObserver';
+
+type UseGetAllQuery<T, K> = UseQuery<
+  QueryDefinition<
+    unknown,
+    ({
+      body
+    }: {
+      body: string;
+    }) => Promise<{
+      data: K;
+    }>,
+    never,
+    T[],
+    string,
+    Record<string, unknown>
+  >
+>;
 
 export default function ItemsGrid<T, K>({
   CardComponent,
@@ -15,7 +34,7 @@ export default function ItemsGrid<T, K>({
 }: {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   CardComponent: React.ComponentType<any>;
-  useGetAllQuery: UseGridOptions<T, K>['useGetAllQuery'];
+  useGetAllQuery: UseGetAllQuery<T, K>;
   xs: GridSize;
   sm: GridSize;
   md: GridSize;
@@ -26,12 +45,56 @@ export default function ItemsGrid<T, K>({
   const pageSize = 20;
   const startPage = 1;
 
-  // use hook for handle the fetch api request with infinite loading
-  const { page, isLastPage, data: characters, paginatedData, error, isFetching, isLoading, containerRef } = useGrid({
-    pageSize,
-    startPage,
-    useGetAllQuery
+  // current page number state
+  const [page, setPage] = useState(startPage);
+
+  // state for indicate if the current page is the last
+  const [isLastPage, setIsLastPage] = useState(false);
+
+  // list of loaded pages state
+  const [loadedPages, setLoadedPages] = useState<number[]>([]);
+
+  // list of loaded characters state
+  const [paginatedData, setPaginatedData] = useState<T[]>(Array.from(new Array(pageSize)));
+
+  // fetch data and get the status
+  const { data: fetchedItems, error, isFetching, isLoading } = useGetAllQuery(page);
+
+  // hook for handle infinite loading
+  const [containerRef, loadMore] = useIntersectionObserver({
+    root: null,
+    rootMargin: '600px',
+    threshold: 1.0
   });
+
+  /**
+   * Watch the loadMore flag and set the next page if not yet loaded
+   */
+  useEffect(() => {
+    if (loadMore && !loadedPages.includes(page + 1)) {
+      // added empty item for skeleton loading
+      setPaginatedData((_paginatedData) => _paginatedData.concat(Array.from(new Array(pageSize))));
+
+      // set next page number for fetch new page
+      setPage(page + 1);
+    }
+  }, [loadMore]);
+
+  /**
+   * Watch the data form api and set the paginated data, the loaded pages and the last page flag
+   */
+  useEffect(() => {
+    if (fetchedItems && Array.isArray(fetchedItems)) {
+      // set paginated items with: remove the skeleton element and  append the last page on tail
+      setPaginatedData((_paginatedData) => [..._paginatedData.slice(0, (page - 1) * 20), ...fetchedItems]);
+
+      // set the current page in loaded pages state
+      setLoadedPages((_loadedPages) => [..._loadedPages, page]);
+
+      // set is last page flag state
+      setIsLastPage(fetchedItems.length < pageSize);
+    }
+  }, [fetchedItems]);
 
   if (error) {
     return (
@@ -41,7 +104,7 @@ export default function ItemsGrid<T, K>({
     );
   }
 
-  if (!isFetching && !isLoading && !paginatedData?.length && !characters?.length) {
+  if (!isFetching && !isLoading && !paginatedData?.length && !fetchedItems?.length) {
     return (
       <Container className={classes.cardGrid} maxWidth="md">
         <Alert severity="info">There are no data!</Alert>
@@ -52,22 +115,19 @@ export default function ItemsGrid<T, K>({
   return (
     <Container className={classes.cardGrid} maxWidth="xl">
       <Grid container spacing={4}>
-        {/* show fake cards with skeleton when loading */}
-        {(isFetching || isLoading ? paginatedData.concat(Array.from(new Array(pageSize))) : paginatedData).map(
-          (item: T, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <Grid item key={index} xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <CardComponent
-                // loading only the new cards
-                isLoading={(isFetching || isLoading) && index >= (page - 1) * pageSize}
-                item={item}
-              />
-            </Grid>
-          )
-        )}
+        {paginatedData.map((item: T, index: number) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Grid item key={index} xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+            <CardComponent
+              // loading only the new cards
+              isLoading={!item}
+              item={item}
+            />
+          </Grid>
+        ))}
       </Grid>
       {/* loading text for known when a user scroll to bottom and load moore data  */}
-      {!isLastPage ? (
+      {!isLastPage && !isFetching ? (
         <p ref={containerRef as LegacyRef<HTMLParagraphElement>} className={classes.loadingText}>
           loading...
         </p>
